f75fef46d9366ff4a1319fc7c7c072e9733a16e8
andrea2801 <71711841+andrea2801@users.noreply.github.com>
2021-06-04 14:32:30 +0200

    fix(login validate html)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     @yield('title')
     <title>DeltaSAD</title>
     <link rel="icon" type="image/png" href="{{asset('img/Logo2.png')}}" sizes="64x64">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
