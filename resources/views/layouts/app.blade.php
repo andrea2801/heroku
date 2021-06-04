e98ac440e3cc3fb3a3b51aecb85006f8723c2237
andrea2801 <71711841+andrea2801@users.noreply.github.com>
2021-06-04 14:45:30 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,17 +15,22 @@
     <link href="{{ asset('admin/css/bootstrap.css') }}" rel="stylesheet">
     <link href="{{ asset('admin/css/bootstrap.min.css') }}" rel="stylesheet">
 
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+   <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+   <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
 
 </head>
 <body class="login_body">
     @yield('content')
 
-    <script  src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
     <script  src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 
     <script  src="{{ asset('front/js/main.js') }}"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="{{ asset('front/js/bootstrap.min.js') }}"></script>
+    <script  src="{{ asset('front/js/bootstrap.min.js') }}"></script>
 
     @yield('scripts')
 </body>
