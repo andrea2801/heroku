98bafe8fe3e70ea346ca6a587da4e75c5e4022eb
andrea2801 <71711841+andrea2801@users.noreply.github.com>
2021-06-04 15:50:17 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,6 +23,8 @@
 
 </head>
 <body class="login_body">
+<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
+<img alt="Licencia de Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />Este obra está bajo una <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">licencia de Creative Commons Reconocimiento-CompartirIgual 4.0 Internacional</a>.
     @yield('content')
 
     <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
